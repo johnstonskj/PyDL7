466d77f0df6ba32c63298dbb17596cee4c92e389
Johnston <johnstonskj@gmail.com>
2016-05-18 19:04:49 -0700

    Adding tests for JSON writer

--- divelog/json/__init__.py
+++ divelog/json/__init__.py
@@ -3,6 +3,7 @@ This module implements a format handler that writes log data into
 JSON in a direct representation of the Log objects in the divelog
 module.
 """
+import datetime
 import json
 
 FORMAT_NAME = 'JSON'
@@ -13,7 +14,7 @@ def parse(file):
     Parse this file object and return either a new top-level Log
     object, or None.
     """
-    pass
+    raise NotImplementedError()
 
 
 def dump(log, file):
@@ -21,9 +22,12 @@ def dump(log, file):
     Serialize the log to the provided file object.
     """
     def to_dict(obj):
-        d = obj.__dict__
-        if 'metadata' in d:
-            del d['metadata']
-        return d
+        if isinstance(obj, datetime.datetime):
+            return obj.timestamp()
+        else:
+            d = obj.__dict__
+            if 'metadata' in d:
+                del d['metadata']
+            return d
 
-    json.dump(log, file, default=to_dict, indent=2)
+    json.dump(log, file, default=to_dict)
---
+++ tests/test_json.py
@@ -0,0 +1,21 @@
+import io
+import pytest
+
+import divelog.json
+
+import examples
+
+
+def test_json_parser():
+    with pytest.raises(NotImplementedError):
+        divelog.json.parse(None)
+
+
+def test_json_writer():
+    log = examples.create_log()
+    file = io.StringIO()
+    divelog.json.dump(log, file)
+    content = file.getvalue()
+    assert '"leave_surface_time": 1457204400.0' in content
+    assert '"reach_surface_time": 1457205000.0' in content
+    assert '"computer_serial": "8762-3478-6234"' in content
